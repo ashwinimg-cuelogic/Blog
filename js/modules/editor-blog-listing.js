5a0e1889732e169047e2605aea41a6d557c8790c
ashwinimg-cuelogic <ashwini.gawade@cuelogic.co.in>
2015-06-11 20:26:36 +0530

    completed with the editor-blog-listing with pagination, search bar, delete modal.

---
+++ js/modules/editor-blog-listing.js
@@ -0,0 +1,11 @@
+(function(){	
+	//triggered when modal is about to be shown
+	$('#blog-delete-modal').on('show.bs.modal', function(e) {
+
+	    //get data-id attribute of the clicked element
+	    var title = $(e.relatedTarget).parents().find('.panel-title').html();
+
+	    //populate the textbox
+	    $(e.currentTarget).find('.modal-body p').text('Are you sure you want to delete the post "'+title+ ' "?');
+	});
+})();
